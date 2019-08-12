9187d4d0a07038e5a2c118a75aba7caf324c15c6
chenjinhuo <1443970739@qq.com>
2019-08-12 17:55:16 +0800

    feat：更改地址

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 // const baseUrl = 'http://localhost:8080'
-const baseUrl = 'http://45.77.87.244:8080/'
+const baseUrl = 'http://45.77.87.244:8080'
 export default new Vuex.Store({
   state: {
     APIS: {
