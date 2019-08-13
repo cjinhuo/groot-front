a42094fe685fa3b12287e82522e3e2b2d080b530
chenjinhuo <1443970739@qq.com>
2019-08-13 15:28:40 +0800

    feat：添加clipBorad插件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,16 +4,21 @@
       tab-position="right"
       style="height: 100%;"
       v-model="activeTabName"
+      v-loading="loading"
+      element-loading-text="疯狂加载中"
+      element-loading-spinner="el-icon-loading"
     )
       el-tab-pane(label="配置项" name="config")
         FormConfig(@emitGetListWithParams="receiveGetListWithParams" @emitFormData="receiveFormData")
       el-tab-pane(label="接口列表" name="list")
         List(:apiList="apiList" @emitGenerateCode="receiveGenerateCode")
       el-tab-pane(label="代码模板" name="template")
+        el-button(type="primary" id="copy-code" :data-clipboard-text="codes" @click="onClickCopy") 复制
         CodeTemplate(:codes="codes")
 </template>
 
 <script>
+import Clipboard from 'clipboard'
 import FormConfig from './FormConfig'
 import List from './List'
 import CodeTemplate from './CodeTemplate'
@@ -35,7 +40,8 @@ export default {
         postFormatter: '',
         url: ''
       },
-      codes: ''
+      codes: '',
+      loading: false
     }
   },
   computed: {
@@ -44,6 +50,23 @@ export default {
     }
   },
   methods: {
+    onClickCopy() {
+      let clipBoard = new Clipboard('#copy-code')
+      clipBoard.on('success', e => {
+        this.$message({
+          message: '复制成功',
+          type: 'success'
+        })
+        clipBoard.destroy()
+      })
+      clipBoard.on('error', e => {
+        this.$message({
+          message: '复制失败',
+          type: 'error'
+        })
+        clipBoard.destroy()
+      })
+    },
     receiveGenerateCode(include) {
       const params = Object.assign({}, this.buildParams, {include})
       this.requestBuild(params)
@@ -56,7 +79,9 @@ export default {
       this.requestListWithUrl(this.buildParams.url)
     },
     async requestListWithUrl(url) {
+      this.loading = true
       const { data } = await Axios.get(`${this.APIS.list}?url=${url}`)
+      this.loading = false
       if (data.success) {
         this.apiList = data.data
       } else {
@@ -67,10 +92,12 @@ export default {
       }
     },
     async requestBuild(params) {
+      this.activeTabName = 'template'
+      this.loading = true
       const { data } = await Axios.post(`${this.APIS.build}`, params)
+      this.loading = false
       if (data.success) {
         this.codes = data.data
-        this.activeTabName = 'template'
       } else {
         this.$message({
           message: data.message,
