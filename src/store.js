4b8d427707d04799737e0c6e3025f636460d4c46
chenjinhuo <1443970739@qq.com>
2019-09-27 14:29:56 +0800

    feat:增加cypress

--- src/store.js
+++ src/store.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-// const baseUrl = 'http://localhost:9999'
-const baseUrl = 'http://45.77.87.244:8080'
+const baseUrl = 'http://localhost:80'
+// const baseUrl = 'http://45.77.87.244:8080'
 export default new Vuex.Store({
   state: {
     APIS: {
